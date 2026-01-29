633d2943428c1103cbcf7afe43b51c922b22fb1b
Yousef <karty@uwindsor.ca>
2026-01-28 23:25:08 -0500

    add input fields

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
-#include "mainwindow.h"
-
 #include <QApplication>
 #include <QWidget>
 #include <QDoubleSpinBox>
 #include <QLabel>
 #include <QVBoxLayout>
 
+#define INIT_YEAR 0
+#define INIT_RISK 0.1
 #define INIT_PRICE 100.0
+#define INIT_MU 5
+#define INIT_SIGMA 0.01
+#define INIT_VOLATILITY 0.2
 
 int main(int argc, char *argv[])
 {
@@ -14,16 +17,46 @@ int main(int argc, char *argv[])
 
     QWidget w;
     w.setWindowTitle("Black Scholes Model");
+    w.resize(600,400);
+
+    auto *spinT = new QDoubleSpinBox();
+    spinT->setRange(0,1000);
+    spinT->setValue(INIT_YEAR);
+    spinT->setPrefix("T = ");
+
+    auto *spinR = new QDoubleSpinBox();
+    spinR->setRange(0,1);
+    spinR->setValue(INIT_RISK);
+    spinR->setPrefix("r = ");
 
     auto *spinS = new QDoubleSpinBox();
     spinS->setRange(0,1000000);
     spinS->setValue(INIT_PRICE);
     spinS->setPrefix("S = ");
 
+    auto *spinMu = new QDoubleSpinBox();
+    spinMu->setRange(0,1000000);
+    spinMu->setValue(INIT_MU);
+    spinMu->setPrefix(QString::fromUtf8(u8"\u03BC = "));
+
+    auto *spinSigma = new QDoubleSpinBox();
+    spinSigma->setRange(0,1);
+    spinSigma->setValue(INIT_SIGMA);
+    spinSigma->setPrefix(QString::fromUtf8(u8"\u03C3 = "));
+
+    auto *spinA = new QDoubleSpinBox();
+    spinA->setRange(0,1);
+    spinA->setValue(INIT_VOLATILITY);
+    spinA->setPrefix("a = ");
+
     auto *priceLabel = new QLabel(QString("Price: %1").arg(INIT_PRICE));
 
     auto *layout = new QVBoxLayout();
     layout->addWidget(spinS);
+    layout->addWidget(spinR);
+    layout->addWidget(spinMu);
+    layout->addWidget(spinSigma);
+    layout->addWidget(spinA);
     layout->addWidget(priceLabel);
     w.setLayout(layout);
 
